3b8d01f4858ffa78d9cc66b5161a7bdf8064e05a
deboralagemb <deboralagemb@gmail.com>
2021-04-15 15:16:20 -0300

    add AddAccount use case

---
+++ CleanApp/Domain/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Débora Lage on 15/04/21.
+//
+
+import Foundation
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
