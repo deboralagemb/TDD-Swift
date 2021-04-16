79e4e42534d2d2a248fa84dd67c0d06f4e8f2559
deboralagemb <deboralagemb@gmail.com>
2021-04-16 15:23:45 -0300

    move production code to their own files

---
+++ CleanApp/Data/HttpPostClient.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Débora Lage on 16/04/21.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?)
+}
