5c89fdcecdc04d1a1a7565e0be74028bb564d520
deboralagemb <deboralagemb@gmail.com>
2021-04-16 15:21:06 -0300

    add Model protocol with toData extension

---
+++ CleanApp/Domain/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  Domain
+//
+//  Created by Débora Lage on 16/04/21.
+//
+
+import Foundation
+
+public protocol Model: Encodable {}
+
+public extension Model {
+    func toData() -> Data? {
+        return try? JSONEncoder().encode(self)
+    }
+}
