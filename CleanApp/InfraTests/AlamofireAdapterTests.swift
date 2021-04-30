d03a0c45ae9fb131440e3097d19ba9fabbe97255
deboralagemb <deboralagemb@gmail.com>
2021-04-30 10:37:20 -0300

    add infra layer

---
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  AlamofireAdapterTests.swift
+//  InfraTests
+//
+//  Created by Débora Lage on 30/04/21.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+    func test_() throws {
+        
+    }
+}
