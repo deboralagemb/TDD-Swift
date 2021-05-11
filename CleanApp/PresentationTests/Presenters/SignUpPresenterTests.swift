6772a5243fe12b7e112317abe7dd523c32ad20b5
deboralagemb <deboralagemb@gmail.com>
2021-05-11 19:07:40 -0300

    ensure SignUpPresenter shows error message when email is invalid

--- CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
+++ CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -50,6 +50,15 @@ class SignUpPresenterTests: XCTestCase {
         sut.signup(viewModel: signUpViewModel)
         XCTAssertEqual(emailValidatorSpy.email, signUpViewModel.email)
     }
+
+    func test_signup_should_show_error_message_if_invalid_email_is_provided() throws {
+        let (sut, alertViewSpy, emailValidatorSpy) = makeSut()
+        let signUpViewModel = SignUpViewModel(name: "any_name", email: "invalid_email@email.com", password: "any_password", passwordConfirmation: "any_password")
+        emailValidatorSpy.isValid = false
+        sut.signup(viewModel: signUpViewModel)
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "Email inválido"))
+    }
+
 }
 
 extension SignUpPresenterTests {
