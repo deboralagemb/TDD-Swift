acf60e81656553f589cca83ce20288414c297710
deboralagemb <deboralagemb@gmail.com>
2021-05-12 21:00:29 -0300

    add UI Layer

---
+++ CleanApp/UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewControllerTests.swift
+//  UITests
+//
+//  Created by Débora Lage on 12/05/21.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+    func test_() throws {
+        
+    }
+}
