4e69d66fa54fc2beea8a67d1b79e7e95a9c2d670
deboralagemb <deboralagemb@gmail.com>
2021-05-12 22:34:07 -0300

    move UIExtension to a separate file

---
+++ CleanApp/UITests/Helpers/TestExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  TestExtensions.swift
+//  UITests
+//
+//  Created by Débora Lage on 12/05/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
