eecc8cf1c303924ff8256b00620958b5e59e4d9c
deboralagemb <deboralagemb@gmail.com>
2021-05-15 17:02:57 -0300

    ensure SignUpPresenter has no memory leak with SignUpViewController

---
+++ CleanApp/MainTests/SignUpIntegrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpIntegrationTests.swift
+//  MainTests
+//
+//  Created by Débora Lage on 13/05/21.
+//
+
+import XCTest
+import Main
+
+class SignUpIntegrationTests: XCTestCase {
+    func test_ui_presentation_integration() {
+        let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
+        checkMemoryLeak(for: sut)
+    }
+}
