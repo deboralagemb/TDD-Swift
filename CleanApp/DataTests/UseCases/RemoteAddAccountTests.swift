c6a0ff438caaf7fca32646eb2e4ce2a720f18ac4
deboralagemb <deboralagemb@gmail.com>
2021-05-16 11:47:52 -0300

    ensure RemoteAddAccount completes with emailInUse error when HttpClient completes with forbidden error

--- CleanApp/DataTests/UseCases/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/UseCases/RemoteAddAccountTests.swift
@@ -31,6 +31,13 @@ class RemoteAddAccountTests: XCTestCase {
         })
     }
     
+    func test_add_should_complete_with_email_if_client_completes_with_forbidden() {
+        let (sut, httpClientSpy) = makeSut()
+        expect(sut, completeWith: .failure(.emailInUse), when: {
+            httpClientSpy.completeWithError(.forbidden)
+        })
+    }
+    
     func test_add_should_complete_with_account_if_client_completes_with_valid_data() {
         let (sut, httpClientSpy) = makeSut()
         let account = makeAccountModel()
