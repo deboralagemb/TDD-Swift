48b95b00b7aebcf4204c0185d269ee7bb92b3350
deboralagemb <deboralagemb@gmail.com>
2021-05-17 21:48:19 -0300

    rename test file

--- CleanApp/MainTests/Factories/SignUpControllerFactoryTests.swift
+++ CleanApp/MainTests/Factories/SignUpControllerFactoryTests.swift
@@ -1,5 +1,5 @@
 //
-//  SignUpFactoryTests.swift
+//  SignUpControllerFactoryTests.swift
 //  MainTests
 //
 //  Created by Débora Lage on 13/05/21.
@@ -10,7 +10,7 @@ import Main
 import UI
 import Validation
 
-class SignUpFactoryTests: XCTestCase {
+class SignUpControllerFactoryTests: XCTestCase {
     func test_background_request_should_complete_on_main_thread() {
         let (sut, addAccountSpy) = makeSut()
         sut.loadViewIfNeeded()
@@ -34,7 +34,7 @@ class SignUpFactoryTests: XCTestCase {
     }
 }
 
-extension SignUpFactoryTests {
+extension SignUpControllerFactoryTests {
     func makeSut(file: StaticString = #file, line: UInt = #line) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy) {
         let addAccountSpy = AddAccountSpy()
         let sut = makeSignUpController(addAccount: MainQueueDispatchDecorator(addAccountSpy))
