a83ebf07a714299601183efe9162cf3139fefc6e
deboralagemb <deboralagemb@gmail.com>
2021-05-18 16:30:59 -0300

    ensure WelcomRouter calls nav with signUpFactory on goToSignUp

--- CleanApp/UITests/Scenes/Welcome/WelcomeRouterTests.swift
+++ CleanApp/UITests/Scenes/Welcome/WelcomeRouterTests.swift
@@ -12,14 +12,20 @@ import UI
 public final class WelcomeRouter {
     private let nav: NavigationController
     private let loginFactory: () -> LoginViewController
+    private let signUpFactory: () -> SignUpViewController
     
-    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController) {
+    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController, signUpFactory: @escaping () -> SignUpViewController) {
         self.nav = nav
         self.loginFactory = loginFactory
+        self.signUpFactory = signUpFactory
     }
     
     public func goToLogin() {
         nav.pushViewController(loginFactory())
+    }
+    
+    public func goToSignUp() {
+        nav.pushViewController(signUpFactory())
     }
 }
 
@@ -30,13 +36,21 @@ class WelcomeRouterTests: XCTestCase {
         XCTAssertEqual(nav.viewControllers.count, 1)
         XCTAssertTrue(nav.viewControllers[0] is LoginViewController)
     }
+    
+    func test_gotoSignUp_calls_nav_with_correct_vc() {
+        let (sut, nav) = makeSut()
+        sut.goToSignUp()
+        XCTAssertEqual(nav.viewControllers.count, 1)
+        XCTAssertTrue(nav.viewControllers[0] is SignUpViewController)
+    }
 }
 
 extension WelcomeRouterTests {
     func makeSut() -> (sut: WelcomeRouter, nav: NavigationController) {
         let loginFactorySpy = LoginFactorySpy()
+        let signUpFactorySpy = SignUpFactorySpy()
         let nav = NavigationController()
-        let sut = WelcomeRouter(nav: nav, loginFactory: loginFactorySpy.makeLogin)
+        let sut = WelcomeRouter(nav: nav, loginFactory: loginFactorySpy.makeLogin, signUpFactory: signUpFactorySpy.makeSignUp)
         return (sut, nav)
     }
 }
@@ -45,6 +59,12 @@ extension WelcomeRouterTests {
     class LoginFactorySpy {
         func makeLogin() -> LoginViewController {
             return LoginViewController.instantiate()
+        }
+    }
+    
+    class SignUpFactorySpy {
+        func makeSignUp() -> SignUpViewController {
+            return SignUpViewController.instantiate()
         }
     }
 }
